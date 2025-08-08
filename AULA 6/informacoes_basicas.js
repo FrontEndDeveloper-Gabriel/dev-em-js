8461ebe5285a34ddf2ed5ba1ac0035824175e775
Gabriel Ribeiro Silva <123027022+FrontEndDeveloper-Gabriel@users.noreply.github.com>
2025-08-08 15:09:03 -0300

    Add files via upload

---
+++ AULA 6/informacoes_basicas.js
@@ -0,0 +1,23 @@
+// COMENTÁRIO DE LINHA
+
+/*
+
+PULAR LINHA
+
+*/
+
+// ESTRUTURA DE DADO
+let numero_que_varia = 20
+const numero_fixo = 30
+
+
+
+// ESTRUTURA DE DADO
+// - REAL (FLOAT) - 1.0, 2.0, 3.0 - USOS: DINHEIRO, PESO E ALTURA.
+let peso = 65.12
+// - BOLEANO (BOOL) - TRUE OR FALSE 1/0 - USO: CONDIÇÕES.
+let x = true
+// - INTEIRO (INT) - 1, 2, 3 - USOS: ALTURA, ANO E NÚMERO DE FILHOS.
+let idade = 25
+// - TEXTO (STRING) - "Isso é um texto!"
+let texto = "Isso é um texto!"
